a4e81897a09971130fae4130299867a67fec9f3e
mxplz27 <palencialopez@gmail.com>
2026-02-20 23:22:41 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Reac-trim3/',  // 👈 fuera de babel
+  base: '/Reac-trimestre3/',  // 👈 fuera de babel
   plugins: [
     react({
       babel: {
